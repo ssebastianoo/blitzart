0e08b4260ca8e4d40073037f33f959dae071ddbb
ssebastianoo <sebastianogirotto05@gmail.com>
2021-12-12 13:29:19 +0100

    it works now

--- script.js
+++ script.js
@@ -1,50 +1,103 @@
-let shelf = document.getElementById('shelf');
-let shelf2 = document.getElementById('shelf2');
-let medias = ["https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Logo_TVE-1.svg/1200px-Logo_TVE-1.svg.png", "https://upload.wikimedia.org/wikipedia/commons/7/75/Logo_TVE-2.svg", "https://media.istockphoto.com/photos/number-3-3d-clean-blue-isolated-on-white-picture-id1019544808?k=20&m=1019544808&s=170667a&w=0&h=7yuA5i8bRJ7T4MQ_aaIIVieuT1e4RWAQ_M05yvLXVdY=", "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/NYCS-bull-trans-4.svg/1024px-NYCS-bull-trans-4.svg.png", "https://thumbs.dreamstime.com/b/3d-numero-5-vetro-5212805.jpg"]
+let shelves = document.getElementById('shelves');
+const lines = 8;
+const scrollSpeed = 2;
+let imgOnTop = document.getElementById('imgOnTop');
+let closeButton = document.getElementById('close');
+let medias = [
+    "https://images.unsplash.com/photo-1639147591951-07df749c760b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639202293330-5f8437183fd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639182946622-7de9d7efa6b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxN3x8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639154872768-355420897b41?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639161042430-2d5ebacc40b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639156299871-a1ffbf7946b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639164666181-d0d9e2cee51b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639180724511-cfd35fe65305?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639165734963-63e358fc5f17?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1638741451679-eb5fa54bce9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyM3x8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639157022525-560bedcbb628?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyN3x8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639175661184-33f1eb06dd29?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0MXx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639170126730-2a09d100e129?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639193910816-d3a259ecb61a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639062532911-5a6e9591d21c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzM3x8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639166248278-04720caa884d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2Nnx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639153519880-d78f6404abe0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639146856276-9ee379bb4dd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639243516757-0f0a659a821b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639156290987-1d156b82a1ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639142982207-8a27b0613298?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639250382548-4ee9396f5372?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639112089287-235c3efef652?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639275617881-d4d39df84950?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639221521531-653e5a57833f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxN3x8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
+    "https://images.unsplash.com/photo-1639242585400-f7029d3eb17c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60"
+]
 
-function init() {
-    medias.forEach((media) => {
-        let img = document.createElement('img');
-        img.src = media;
-        shelf.appendChild(img);
-        shelf2.appendChild(img);
-        shelf2.scrollLeft = shelf.scrollLeftMax;
+const isOdd = num => num % 2 == 1;
+let imgNumber = 0;
+let shelfDiv = document.createElement('div');
+shelfDiv.classList.add('shelf');
+shelves.appendChild(shelfDiv);
+
+medias.forEach((media) => {
+    let img = document.createElement('img');
+    img.src = media;
+
+    img.addEventListener('click', () => {
+        showImgOnTop(media);
     });
-}   
 
-init();
+    imgNumber++;
+    if (imgNumber > lines) {
+        imgNumber = 1;
+        shelfDiv = document.createElement('div');
+        shelfDiv.classList.add('shelf');
+        shelves.appendChild(shelfDiv);
+    } 
+    shelfDiv.appendChild(img);
+});
 
-let x = 0;
-let x2 = shelf2.scrollLeftMax
-setInterval(() => {
-    shelf.scroll({left: x, behavior: 'smooth'});
-    shelf2.scroll({left: x2, behavior: 'smooth'});
-    x += 5;
-    x2 -= 5;
-    if (x >= shelf.scrollLeftMax) {
-        x = 0;
-    }
+function showImgOnTop(url) {
+    imgOnTop.children[0].src = url;
+    imgOnTop.style.display = 'unset';
+    setTimeout(() => {
+        imgOnTop.classList.remove('hidden');
+    }, 100);
+}
 
-    if (x2 <= 0) {
-        x2 = shelf2.scrollLeftMax;
-    }
-}, 100);
+closeButton.addEventListener('click', () => {
+    imgOnTop.classList.add('hidden');
+    setTimeout(() => {
+        imgOnTop.style.display = 'none';
+    }, 500);
+});
 
-/*
-let repeat = 0;
-setTimeout(() => {
-    setInterval(() => {
-        let img = document.createElement('img');
-        img.src = medias[repeat];
-        shelf.appendChild(img);
-        repeat++;
-
-        if (repeat == medias.length) {
-            repeat = 0;
+window.onload = () => {
+    for (i=0; i<shelves.children.length; i++) {
+        if (isOdd(i)) {
+            shelves.children[i].scrollLeft = shelves.children[i].scrollWidth - shelves.children[i].clientWidth;
+        } else {
+            shelves.children[i].scrollLeft = 0;
         }
+    }
 
-        // shelf.children[0].remove();
-        console.log(shelf.children.length);
-    }, 1000);
-}, 2000);
-*/
+    let mode = 'left';
+    let x = 0;
+    let x2 = 0;
+    let pos = new Object();
+    let started = false;
+    setInterval(() => {
+        for (let i = 0; i < shelves.children.length; i++) {
+            if (!pos[i] || pos[i] > (shelves.children[i].scrollWidth - shelves.children[i].clientWidth)) {
+                pos[i] = 0;
+            }
+
+            pos[i] += shelves.children.length/scrollSpeed;
+
+            if (isOdd(i)) {
+                shelves.children[i].scroll({left: shelves.children[i].scrollWidth-shelves.children[i].clientWidth-pos[i], behavior: 'smooth'});
+            } else {
+                shelves.children[i].scroll({left: pos[i], behavior: 'smooth'});
+            }            
+        }
+    }, 100);
+}
